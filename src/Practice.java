90cf3f69bef47da59b9f115a72fdf873a240a7eb
KJ <kjmiclea@gmail.com>
2026-09-22 17:49:27 -0700

    Completed levelCount()

--- src/Practice.java
+++ src/Practice.java
@@ -130,7 +130,11 @@ public class Practice {
      * @return the number of levels in the tree
      */
     public static int levelCount(BinaryTreeNode<?> root) {
-        return 0;
+        if (root == null) return 0;
+        int left = levelCount(root.left);
+        int right = levelCount(root.right);
+        if (left > right) return left + 1;
+        else return right + 1;
     }
 
 
@@ -158,6 +162,10 @@ public class Practice {
      * @return the sum of the nodes at the given level
      */
     public static int sumAtLevel(BinaryTreeNode<Integer> root, int level) {
+        if (root == null) return 0;
+
+        
+
         return 0;
     }
 
